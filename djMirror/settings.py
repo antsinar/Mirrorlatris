5af970f1e1639b7c1bb84853f2d68550e04d4a87
Antsinar <tsinaridisan@gmail.com>
2025-01-20 14:15:44 +0200

    starting django project setup

---
+++ djMirror/settings.py
@@ -0,0 +1,111 @@
+from pathlib import Path
+from os import environ
+from dotenv import find_dotenv, load_dotenv
+
+env = find_dotenv(".env")
+load_dotenv(env)
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+SECRET_KEY = environ.get(
+    "SECRET_KEY", "django-insecure-+6=uy%p5*$s0r8jb)f28mw_vvg(l%+sq9!g^x=)ub42xku&@aw"
+)
+
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+DJANGO_APPS = [
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "django.contrib.sites",
+]
+
+THIRD_PARTY_APPS = []
+
+CORE_APPS = []
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + CORE_APPS
+
+if DEBUG:
+    INSTALLED_APPS.insert(0, "whitenoise.runserver_nostatic")
+
+SITE_ID = 1
+
+MIDDLEWARE = [
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+ROOT_URLCONF = "djMirror.urls"
+
+TEMPLATES = [
+    {
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [BASE_DIR / "templates"],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = "djMirror.wsgi.application"
+ASGI_APPLICATION = "djMirror.asgi.application"
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "dev.sqlite3",
+    }
+}
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
+]
+
+LANGUAGE_CODE = "en-us"
+
+TIME_ZONE = "UTC"
+
+USE_I18N = True
+
+USE_TZ = True
+
+STATIC_URL = "staticfiles/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_DIRS = [
+    BASE_DIR / "staticfiles_src",
+]
+
+STORAGES = {
+    "staticfiles": {"BACKEND": "whitenoise.storage.CompressedStaticFileStorage"}
+}
+
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
